42ea829c94e09b4a443109d5da66dc843c940f98
Murtaza Jafferji <murtazajafferji@gmail.com>
2011-02-04 11:52:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hidb::Application.config.secret_token = '3f96bd61cec0b652a703446a140dcd2aca4eb7aa116ebf749f00cb366ab44b3237660e862faf9306c1eca1e8611cfc8c2802929d66ef11c25d0b77ec3624a836'
