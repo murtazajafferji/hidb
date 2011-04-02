80f76c922611f60473d9f318f8ac885a5af0b24b
Murtaza Jafferji <murtazajafferji@gmail.com>
2011-04-02 04:00:13 -0400

    added facebook

--- app/controllers/internships_controller.rb
+++ app/controllers/internships_controller.rb
@@ -1,5 +1,5 @@
 class InternshipsController < ApplicationController
-  before_filter :require_user, :only => [:create, :update, :destroy]
+  before_filter :require_user, :only => [:create, :update, :destroy, :new]
   before_filter :require_admin, :only => [:unapproved]
   # GET /internships
   # GET /internships.xml
