c6bae0a76ca7a2ec931403779016edf24a98780a
Murtaza Jafferji <murtazajafferji@gmail.com>
2011-04-03 09:21:14 -0400

    facebook like box

--- app/models/user.rb
+++ app/models/user.rb
@@ -273,7 +273,7 @@ class User < ActiveRecord::Base
           :port                 => '587',
           :enable_starttls_auto => true,
           :user_name            => SITE_EMAIL,
-          :password             => 'default',
+          :password             => 'temporary',
           :authentication       => :plain # :plain, :login, :cram_md5, no auth by default
           #:domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
         } 
