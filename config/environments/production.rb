de8185b8a40183f0284401f971c8556d8e01b754
Murtaza Jafferji <murtazajafferji@gmail.com>
2011-04-08 09:22:25 -0400

    google site verification

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,8 +2,8 @@ Hidb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
   SITE_NAME = "Hiatt Internship Database"
-  SITE_URL = "http://www.hidb.heroku.com"
-  SITE_EMAIL = "noreply@amaltracker.com"
+  SITE_URL = "http://www.internshipdatabase.org"
+  SITE_EMAIL = "noreply@internshipdatabase.org"
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
